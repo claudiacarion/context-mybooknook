d82cadb40864e681e710ae6577b79ba05fe4712e
claudiacarion <claudia.carion@gmail.com>
2026-09-10 22:50:21 +0200

    add shadow to login form

--- components/Login/index.tsx
+++ components/Login/index.tsx
@@ -46,8 +46,7 @@ const Login = () => {
   return (
     <div className="m-auto">
       <form
-        className="bg-sage-light text-center w-80 md:w-100 h-118 p-6 m-auto rounded rounded-br-3xl shadow-[4px_4px_0px_rgba(0,0,0,0.12)]
-"
+        className="bg-sage-light text-center w-80 md:w-100 h-118 p-6 m-auto rounded rounded-br-3xl shadow-[6px_6px_0px_rgba(0,0,0,0.12)]"
         onSubmit={handleLogin}>
         <Image src="/logo.png" alt="logo" width={80} height={80} className="w-24 h-24 m-auto" />
         <h2 className="text-sage-dark font-heading text-[44px] leading-[0.9] md:leading-tight">My BookNook</h2>
@@ -56,7 +55,18 @@ const Login = () => {
           Username:
         </label>
         <input
-          className="field bg-cream w-full p-2 rounded-3xl"
+          className="field
+          bg-cream
+          w-full
+          p-2
+          rounded
+          rounded-br-3xl
+          shadow-[4px_4px_0px_rgba(0,0,0,0.12)]
+          hover:shadow-[6px_6px_0px_rgba(0,0,0,0.12)]
+          hover:-translate-x-0.5
+          hover:-translate-y-0.5
+          transition-all
+          duration-200"
           id="username"
           placeholder="Enter your username"
           onChange={handleUsername}
@@ -66,14 +76,25 @@ const Login = () => {
           Password:
         </label>
         <input
-          className="field bg-cream w-full p-2 rounded-3xl"
+          className="field
+        bg-cream
+          w-full
+          p-2
+          rounded
+          rounded-br-3xl
+          shadow-[4px_4px_0px_rgba(0,0,0,0.12)]
+          hover:shadow-[6px_6px_0px_rgba(0,0,0,0.12)]
+          hover:-translate-x-0.5
+          hover:-translate-y-0.5
+          transition-all
+          duration-200"
           id="password"
           placeholder="Enter your password"
           type="password"
           onChange={handlePassword}
           value={password}
         />
-        <button className="bg-pink text-[14px] px-2 py-1 mb-1 rounded-3xl cursor-pointer hover:scale-95 shadow-[4px_4px_0px_rgba(0,0,0,0.12)]">
+        <button className="bg-pink text-[14px] px-2 py-1 mb-1 rounded-2xl cursor-pointer hover:scale-95 shadow-[4px_4px_0px_rgba(0,0,0,0.12)]">
           Log in
         </button>
         {error && <p className="text-sm text-peach">{error}</p>}
