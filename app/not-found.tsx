b6a1e214ad54242fed625e968d6fc04944cb0021
claudiacarion <claudia.carion@gmail.com>
2026-09-11 12:41:13 +0200

    add 404 page

---
+++ app/not-found.tsx
@@ -0,0 +1,9 @@
+export default function NotFound() {
+  return (
+    <div className="p-4 mb-8 text-center">
+      <h4 className="text-peach font-heading text-[24px] px-6 lg:w-1/2 mx-auto">
+        Looks like you got a little lost in the pages. Let’s find our way back to the story.
+      </h4>
+    </div>
+  );
+}
