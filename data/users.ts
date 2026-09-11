68b2f5fc2712cadd29103af9ee4755fe9009934e
claudiacarion <claudia.carion@gmail.com>
2026-09-11 17:22:33 +0200

    add user with default values

--- data/users.ts
+++ data/users.ts
@@ -2,5 +2,18 @@ import { UserType } from "../types/types";
 
 export const users: UserType[] = [
   { username: "rob", password: "ert", genre: null, books: [] },
-  { username: "jane", password: "austin", genre: null, books: [] },
+  { username: "jane", password: "austen", genre: null, books: [] },
+  {
+    username: "mary",
+    password: "shelley",
+    genre: "Romance",
+    books: [
+      {
+        id: "OL450063W",
+        title: "Frankenstein; or, The Modern Prometheus",
+        author: ["Mary Shelley"],
+        cover: "https://covers.openlibrary.org/b/id/12356249-M.jpg",
+      },
+    ],
+  },
 ];
